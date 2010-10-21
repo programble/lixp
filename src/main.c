521a61eca089e6df625f22c70d9f6e4f071a5d0e
Curtis (Programble) <programble@gmail.com>
2010-10-21 19:00:27 -0400

    Added inspect test

---
+++ src/main.c
@@ -0,0 +1,33 @@
+/*  Copyright 2010 Curtis McEnroe <programble@gmail.com>
+ *
+ *  This file is part of Lixp.
+ *
+ *  Lixp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  Lixp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Lixp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "ast.h"
+
+int main(int argc, char **argv)
+{
+#ifdef DEBUG    
+    VALUE foo = LixpCons_new(LixpBuiltin_new(LixpBuiltin_cdr), LixpKeyword_new("foobar"));
+    char *str = LixpValue_inspect(foo);
+    printf("%s\n", str);
+    free(str);
+#endif    
+    return 0;
+}
