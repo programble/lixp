2bdb5d6d9bc6a6301736dc6b12c7fbe05d87f22d
Curtis (Programble) <programble@gmail.com>
2010-11-07 15:42:45 -0500

    Changed to what we just figured it should be

--- src/strutils.c
+++ src/strutils.c
@@ -32,12 +32,11 @@ int str_has_char(const char *str, char c)
 
 int asprintf(char **ret, const char *format, ...)
 {
-    char *s = GC_MALLOC_ATOMIC(1);
     va_list ap;
     va_start(ap, format);
-    int length = vsnprintf(s, 1, format, ap);
-    s = GC_REALLOC(s, length + 1);
-    vsnprintf(s, length, format, ap);
+    int length = vsnprintf(NULL, 0, format, ap);
+    char *s = GC_MALLOC_ATOMIC(length + 1);
+    vsnprintf(s, length + 1, format, ap);
     va_end(ap);
     *ret = s;
     /* ? */
